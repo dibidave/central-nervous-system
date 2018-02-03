6752d6db5aa1bb3df0a89c1bc06bac24f9f48adf
David Brown <dibidave@gmail.com>
2018-02-03 15:25:20 -0800

    Allow not one-hot-encoding for getting the data, added dumb random forest example

--- project1/utils.py
+++ project1/utils.py
@@ -3,12 +3,13 @@ from sklearn.model_selection import train_test_split
 from keras.utils.np_utils import to_categorical
 
 
-def get_training_data(validation_size=0.3):
+def get_training_data(validation_size=0.3, use_one_hot_encoding=True):
     """
     Loads the training data, splits it into training and validation sets, and
     converts the labels into 2-class one-hot encoding
 
     :param validation_size: What ratio of the dataset to use as training data
+    :param use_one_hot_encoding: Whether to use one hot encoding
     :return: (training_features, training_labels,
         validation_features, validation_labels)
     """
@@ -22,8 +23,9 @@ def get_training_data(validation_size=0.3):
     training_features, validation_features, training_labels, validation_labels = \
         train_test_split(features, labels, test_size=validation_size)
 
-    training_labels = to_categorical(training_labels)
-    validation_labels = to_categorical(validation_labels)
+    if use_one_hot_encoding:
+        training_labels = to_categorical(training_labels)
+        validation_labels = to_categorical(validation_labels)
 
     return (training_features, training_labels,
             validation_features, validation_labels)
