f2a79098b6bb1404c2b89b42c2055a67932b0de0
Jialiang Lu <jllu@caltech.edu>
2018-02-23 00:46:59 -0800

    add genres_similarity and ratings_bayesian

--- project2/utils.py
+++ project2/utils.py
@@ -34,3 +34,36 @@ def list_to_matrix(Y, M=943, N=1682, fill=np.nan):
     for l in Y:
         YM[l[0]-1, l[1]-1] = l[2]
     return YM
+
+def genre_similarity(genres, fill=np.nan):
+    '''
+    Given a list of movies with shape (#movies, #genres), caculate the probability of any movie
+    belonging to the two genres simultaeneously.
+    
+    returns a upper triagular matrix with shape (#genres, #genres)
+    '''
+    M = genres.shape[0]
+    N = genres.shape[1]
+    S = np.full((N, N), fill)
+    for i in range(N):
+        for j in range(i, N):
+            S[i, j] = np.sum(genres[:, i]*genres[:, j])/M
+    return S
+
+def bayesian_rating(Y, thr=5, M=943, N=1682):
+    '''
+    Correct the rating of the movies by the number of the ratings through a bayesian approach.
+    Reference: 
+    https://stats.stackexchange.com/questions/15979/how-to-find-confidence-intervals-for-ratings
+    
+    returns the number of ratings, the original avarage rating and the correctd avarage rating.
+    '''
+    YM = list_to_matrix(Y, M, N)
+    C = np.mean(Y[:, 2]) # Mean rating of all movies
+    counts = np.sum(1-np.isnan(YM), axis=0) # Number of ratings for each movie
+    counts_sum = np.sum(counts)
+    ratings = np.nanmean(YM, axis=0) # Mean rating of the movies
+    ratings_all = np.full((N,), C)
+    weights = counts/(counts+thr)
+    ratings_bayesian = weights*ratings+(1-weights)*ratings_all
+    return counts, ratings, ratings_bayesian
