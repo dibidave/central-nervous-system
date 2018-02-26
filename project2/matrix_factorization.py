b1809b1961ecdfed858b148a7df9c3084a7db827
Jialiang Lu <jllu@caltech.edu>
2018-02-26 01:18:44 -0800

    Added global bias term and used our own function to calculate error

--- project2/matrix_factorization.py
+++ project2/matrix_factorization.py
@@ -1,7 +1,7 @@
 import numpy as np
 np.seterr('raise')
 
-def grad_U(Ui, Yij, Vj, reg, eta, U_i_bias=0, V_j_bias=0):
+def grad_U(Ui, Vj, Yij, reg, eta, U_i_bias=0, V_j_bias=0, global_bias=0):
     """
     Takes as input Ui (the ith row of U), a training point Yij, the column
     vector Vj (jth column of V^T), reg (the regularization parameter lambda),
@@ -10,9 +10,9 @@ def grad_U(Ui, Yij, Vj, reg, eta, U_i_bias=0, V_j_bias=0):
     Returns the gradient of the regularized loss function with
     respect to Ui multiplied by eta.
     """
-    return eta*(reg*Ui-(Yij-(np.dot(Ui, Vj) + U_i_bias + V_j_bias))*Vj)
+    return eta*(reg*Ui-(Yij-global_bias-(np.dot(Ui, Vj)+U_i_bias+V_j_bias))*Vj)
 
-def grad_V(Vj, Yij, Ui, reg, eta, U_i_bias=0, V_j_bias=0):
+def grad_V(Ui, Vj, Yij, reg, eta, U_i_bias=0, V_j_bias=0, global_bias=0):
     """
     Takes as input the column vector Vj (jth column of V^T), a training point Yij,
     Ui (the ith row of U), reg (the regularization parameter lambda),
@@ -21,9 +21,9 @@ def grad_V(Vj, Yij, Ui, reg, eta, U_i_bias=0, V_j_bias=0):
     Returns the gradient of the regularized loss function with
     respect to Vj multiplied by eta.
     """
-    return eta*(reg*Vj-(Yij-(np.dot(Ui, Vj) + U_i_bias + V_j_bias))*Ui)
+    return eta*(reg*Vj-(Yij-global_bias-(np.dot(Ui, Vj)+U_i_bias+V_j_bias))*Ui)
 
-def grad_bias(Vj, Yij, Ui, eta, U_i_bias, V_j_bias):
+def grad_U_bias(Ui, Vj, Yij, reg, eta, U_i_bias=0, V_j_bias=0, global_bias=0):
     """
     Takes as input the column vector Vj (jth column of V^T), a training point Yij,
     Ui (the ith row of U), and eta (the learning rate).
@@ -31,8 +31,27 @@ def grad_bias(Vj, Yij, Ui, eta, U_i_bias, V_j_bias):
     Returns the gradient of the regularized loss function with
     respect to the u_i, v_i bias terms
     """
+    return eta*(reg*U_i_bias-(Yij-global_bias-(np.dot(Ui, Vj)+U_i_bias+V_j_bias)))
 
-    return eta*(-2 * (Yij-(np.dot(Ui, Vj) + U_i_bias + V_j_bias)))
+def grad_V_bias(Ui, Vj, Yij, reg, eta, U_i_bias=0, V_j_bias=0, global_bias=0):
+    """
+    Takes as input the column vector Vj (jth column of V^T), a training point Yij,
+    Ui (the ith row of U), and eta (the learning rate).
+
+    Returns the gradient of the regularized loss function with
+    respect to the u_i, v_i bias terms
+    """
+    return eta*(reg*V_j_bias-(Yij-global_bias-(np.dot(Ui, Vj)+U_i_bias+V_j_bias)))
+
+#def grad_global_bias(Ui, Vj, Yij, reg, eta, U_i_bias=0, V_j_bias=0, global_bias=0):
+#    """
+#    Takes as input the column vector Vj (jth column of V^T), a training point Yij,
+#    Ui (the ith row of U), and eta (the learning rate).
+#
+#    Returns the gradient of the regularized loss function with
+#        respect to the global bias terms
+#    """
+#    return eta*(-(Yij-global_bias-(np.dot(Ui, Vj)+U_i_bias+V_j_bias)))
 
 def get_err(U, V, Y, reg=0., biases=None):
     """
@@ -44,13 +63,20 @@ def get_err(U, V, Y, reg=0., biases=None):
     estimating Y_{ij} as the dot product of the ith row of U and the jth column of V^T.
     """
     err = 0.
-    for i, j, Yij in Y.tolist():
-        if biases is not None:
-            err += (Yij - (np.dot(U[:, i-1], V[:, j - 1]) +
-                           biases[0][i - 1] + biases[1][j - 1])) ** 2
-        else:
-            err+=(Yij-np.dot(U[:, i-1], V[:, j - 1]))**2
-    return reg/2*(np.linalg.norm(U)+np.linalg.norm(V))+1/2*err
+    if biases is not None:
+        for i, j, Yij in Y.tolist():
+            if i>U.shape[1] or j>V.shape[1]:
+                continue
+            err += (Yij - (np.dot(U[:, i-1], V[:, j-1]) +
+                           biases[0][i - 1] + biases[1][j - 1] + biases[2])) ** 2
+        return reg/2*(np.linalg.norm(U)+np.linalg.norm(V)+
+                      np.linalg.norm(biases[0])+np.linalg.norm(biases[1]))+1/2*err
+    else:
+        for i, j, Yij in Y.tolist():
+            if i>U.shape[1] or j>V.shape[1]:
+                continue
+            err+=(Yij-np.dot(U[:, i-1], V[:, j-1]))**2
+        return reg/2*(np.linalg.norm(U)+np.linalg.norm(V))+1/2*err
 
 
 def train_model(Y, M, N, K, eta=0.03, reg=0., eps=0.0001, max_epochs=300,
@@ -77,7 +103,8 @@ def train_model(Y, M, N, K, eta=0.03, reg=0., eps=0.0001, max_epochs=300,
     if include_bias:
         biases = [
             np.random.uniform(-0.5, 0.5, (M, )),
-            np.random.uniform(-0.5, 0.5, (N, ))
+            np.random.uniform(-0.5, 0.5, (N, )),
+            np.nanmean(Y[:, 2])
         ]
     else:
         biases = None
@@ -92,15 +119,17 @@ def train_model(Y, M, N, K, eta=0.03, reg=0., eps=0.0001, max_epochs=300,
             Ui = U[:, i-1]
             Vj = V[:, j-1]
             if include_bias:
-                dU = grad_U(Ui, Yij, Vj, reg, eta, biases[0][i - 1], biases[1][j - 1])
-                dV = grad_V(Vj, Yij, Ui, reg, eta, biases[0][i - 1], biases[1][j - 1])
-                delta_bias = grad_bias(Vj, Yij, Ui, eta,
-                                       biases[0][i - 1], biases[1][j - 1])
-                biases[0][i - 1] -= delta_bias
-                biases[1][j - 1] -= delta_bias
+                dU = grad_U(Ui, Vj, Yij, reg, eta, biases[0][i - 1], biases[1][j - 1], biases[2])
+                dV = grad_V(Ui, Vj, Yij, reg, eta, biases[0][i - 1], biases[1][j - 1], biases[2])
+                delta_U_bias = grad_U_bias(Ui, Vj, Yij, reg, eta,
+                                       biases[0][i - 1], biases[1][j - 1], biases[2])
+                delta_V_bias = grad_V_bias(Ui, Vj, Yij, reg, eta,
+                                       biases[0][i - 1], biases[1][j - 1], biases[2])
+                biases[0][i - 1] -= delta_U_bias
+                biases[1][j - 1] -= delta_V_bias
             else:
-                dU = grad_U(Ui, Yij, Vj, reg, eta)
-                dV = grad_V(Vj, Yij, Ui, reg, eta)
+                dU = grad_U(Ui, Vj, Yij, reg, eta)
+                dV = grad_V(Ui, Vj, Yij, reg, eta)
             U[:, i-1] = Ui-dU
             V[:, j-1] = Vj-dV
         if epoch==0:
